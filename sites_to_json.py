4014fc88da8d6cd080084c716c3e5772ee014aa2
Charles <charles.halfpenny@gmail.com>
2023-08-05 11:12:39 -0400

    added sites

---
+++ sites_to_json.py
@@ -0,0 +1,31 @@
+import openpyxl
+import json
+
+# Load workbook
+wb = openpyxl.load_workbook(r'C:\Users\Charles\OneDrive\Personal Documents\Glenhaven\sites.xlsx')
+
+# Select active sheet
+sheet = wb.active
+
+# Read data from spreadsheet
+data = []
+for row in sheet.iter_rows(min_row=2, values_only=True):  # min_row=2 to skip header
+    customer_name, site_name, street, city, state, zip_code = row
+    data.append(
+        {
+            'SiteName': site_name, 
+            'Street': street,
+            'City': city,
+            'State': state,
+            'ZipCode': zip_code,
+            'CustomerName': customer_name
+        })
+
+# Write data to JSON file
+#with open('output.json', 'w') as f:
+#    json.dump(data, f, indent=None)
+
+with open('output.json', 'w') as f:
+    for record in data:
+        f.write(json.dumps(record))
+        f.write(',\n')
